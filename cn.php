e76cb78b16afa482d0490b1d6e503afab62f9cef
Edwardpv2123 <62851630+Edwardpv2123@users.noreply.github.com>
2020-04-21 20:48:34 -0500

    Add files via upload

---
+++ cn.php
@@ -0,0 +1,8 @@
+<?php
+$conexion = mysqli_connect("https://edwardpv2123.github.io/conoceatumascota/", "root", "", "bd_prueba");
+//if (!$conexion) {
+//    echo 'Error al conectar a la base de datos';
+//}
+//else {
+//    echo 'Conectado a la base de datos';
+//}
